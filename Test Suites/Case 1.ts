7efc2ae298dec1f1b6321f455889ef168d27cde8
alinaufalammarullah <alinaufalammarullah@caresomdymacair>
2023-01-04 23:05:31 +0700

    automation api

---
+++ Test Suites/Case 1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Case 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2536c30a-11c0-417c-8fa9-cdb5dfaed593</testSuiteGuid>
+   <testCaseLink>
+      <guid>c81d6d58-d9b1-45e8-9fc1-8a96f022c42f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Select Value Email From GET Single User</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
